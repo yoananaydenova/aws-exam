54e0b602043c2aaf06a9327a728bbe778059d1fd
Yoana Naydenova <y.naydenova@gmail.com>
2024-11-24 15:00:56 +0200

    add cleanup function

---
+++ src/cleanupFunction.ts
@@ -0,0 +1,18 @@
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
+import { v4 } from "uuid";
+
+const snsClient = new SNSClient({});
+const dynamoDBClient = new DynamoDBClient({});
+
+export const handler = async (event: any) => {
+    const tableName = process.env.TABLE_NAME;
+    const topicArn = process.env.TOPIC_ARN;
+  
+    console.log(event);
+  
+    return {
+      statusCode: 200,
+      body: "Hi from Lambda!",
+    };
+  };
