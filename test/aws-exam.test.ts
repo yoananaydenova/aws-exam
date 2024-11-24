cbf2d023feb81dd65778822651022aa483abb68a
Yoana Naydenova <y.naydenova@gmail.com>
2024-11-24 09:12:36 +0200

    add initial cdk project files

---
+++ test/aws-exam.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsExam from '../lib/aws-exam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-exam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsExam.AwsExamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
