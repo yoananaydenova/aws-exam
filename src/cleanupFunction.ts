2ba7f75141745c36fd6514068cc4610b589b8a52
Yoana Naydenova <y.naydenova@gmail.com>
2024-11-24 15:06:37 +0200

    fix ceanup function

--- src/cleanupFunction.ts
+++ src/cleanupFunction.ts
@@ -1,18 +1,19 @@
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
-import { v4 } from "uuid";
 
 const snsClient = new SNSClient({});
 const dynamoDBClient = new DynamoDBClient({});
 
 export const handler = async (event: any) => {
-    const tableName = process.env.TABLE_NAME;
-    const topicArn = process.env.TOPIC_ARN;
-  
-    console.log(event);
-  
-    return {
-      statusCode: 200,
-      body: "Hi from Lambda!",
-    };
-  };
+  const tableName = process.env.TABLE_NAME;
+  const topicArn = process.env.TOPIC_ARN;
+
+  await snsClient.send(
+    new PublishCommand({
+      TopicArn: topicArn,
+      Message: `An file deleteded!`,
+    })
+  );
+
+  console.log(event);
+};
